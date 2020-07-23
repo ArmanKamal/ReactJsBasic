c228b02a392f503f1103a9d1b4cbdd417f7679c4
Arman Kamal <armankamal2421@gmail.com>
2020-07-23 10:24:55 -0700

    Functional Component Greet

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Greet from './components/Greet'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Greet />
+    </div>
+  );
+}
+
+export default App;
