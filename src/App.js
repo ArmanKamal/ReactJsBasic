e953eb8d1992d9b6caa76553f3d71fac775fbd3d
Arman Kamal <armankamal2421@gmail.com>
2020-07-23 10:38:52 -0700

    Added Class Component Welcome

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Greet from './components/Greet'
+import Welcome from './components/Welcome'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Greet />
+      <Welcome />
     </div>
   );
 }
