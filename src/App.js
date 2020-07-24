721f5552983a5b85e60c2af6fb439a91933f0ba6
Arman Kamal <armankamal2421@gmail.com>
2020-07-24 04:02:36 -0700

    Message State added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
+import Message from './components/Message'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Bruce" heoName="Batman">
+      {/* <Greet name="Bruce" heoName="Batman">
         <p>This is children props</p>
       </Greet>
       <Greet name="Diana" heroName="SuperWomen"/>
       <Greet name="John" heroName="SuperMan" />
-      <Welcome name="Diana" heroName="SuperWomen"/>
+      <Welcome name="Diana" heroName="SuperWomen"/> */}
+      <Message />
     </div>
   );
 }
