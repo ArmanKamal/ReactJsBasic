e16975614342a15e554bd4c9e911d834f5fbc1b2
Arman Kamal <armankamal2421@gmail.com>
2020-08-21 21:35:02 -0700

    Rendering List using Map

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,12 +1,31 @@
 import React from 'react'
-
+import Person from './Person'
 function NameList() {
+    const persons = [
+    {
+        name: 'Arman',
+        roll: 35,
+        age: 23
+    },
+    {
+        name: 'John',
+        roll: 20,
+        age: 42
+    },  {
+        name: 'Cena',
+        roll: 12,
+        age: 41
+    },
+
+]
     const names = ['Arman','John','Cena','Brain']
-    const nameList = names.map((name) =><h2>Hello {name}</h2> )
+    const nameList = names.map((name) => <h2>{name}</h2>)
+    const personList = persons.map((person) => (<Person person={person} />))
 
     return (
         <div>
             {nameList}
+            {personList}
         </div>
     )
 }
