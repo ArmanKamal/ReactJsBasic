32c7c0c25ee7385a240d60ba4bac6d49beca7e8a
Arman Kamal <armankamal2421@gmail.com>
2020-08-21 22:59:24 -0700

    Map key added

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import Person from './Person'
+
+// Why key is needed 
+
+// Because without key react cant compare with old tree so it has to create a new copy... 
 function NameList() {
     const persons = [
     {
+        id: 1,
         name: 'Arman',
         roll: 35,
         age: 23
     },
     {
+        id: 2,
         name: 'John',
         roll: 20,
         age: 42
     },  {
+        id: 3,
         name: 'Cena',
         roll: 12,
         age: 41
@@ -19,8 +26,8 @@ function NameList() {
 
 ]
     const names = ['Arman','John','Cena','Brain']
-    const nameList = names.map((name) => <h2>{name}</h2>)
-    const personList = persons.map((person) => (<Person person={person} />))
+    const nameList = names.map((name) => <h2 key={name}>{name}</h2>)
+    const personList = persons.map((person) => (<Person key={person.name} person={person} />))
 
     return (
         <div>
